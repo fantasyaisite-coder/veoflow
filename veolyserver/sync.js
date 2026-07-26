8c94db6036e44a333baa7a7a129e58c498c05958
fantasyaisite-coder <fantasyaisite@gmail.com>
2026-07-26 17:33:46 +0530

    Delete veolyserver directory

--- veolyserver/sync.js
+++
@@ -1,7 +0,0 @@
-const { syncAll } = require("./lib/sync");
-
-(async () => {
-  console.log("Starting sync...");
-  const result = await syncAll();
-  console.log(JSON.stringify(result, null, 2));
-})();
